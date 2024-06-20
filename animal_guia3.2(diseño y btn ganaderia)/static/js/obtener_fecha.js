1ce099f240ff1ac9ceafebc48571cf77c9a1dc03
bioguia <bioguia40@gmail.com>
2024-06-19 21:21:33 -0400

    Delete animal_guia3.2(diseño y btn ganaderia) directory

--- animal_guia3.2(diseño y btn ganaderia)/static/js/obtener_fecha.js
+++
@@ -1,8 +0,0 @@
-// Obtener el elemento span donde se mostrará el año
-var yearSpan = document.getElementById("year");
-
-// Obtener el año actual
-var currentYear = new Date().getFullYear();
-
-// Actualizar el contenido del span con el año actual
-yearSpan.textContent = currentYear;
